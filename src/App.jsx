4b87fb62ea02bf2cdbec582cc3f8a70ec853d05d
amanmittal06 <amanagrawal0139@gmail.com>
2025-02-01 17:44:13 +0530

    initialise repo

---
+++ src/App.jsx
@@ -0,0 +1,82 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios';
+
+function App() {
+
+  // const [data, setData]  = useState([]);
+
+  const loadData = async () =>{
+    try{
+      const res =  await axios.get('https://actiplan.vercel.app/chantingCounts/achyuta');
+      console.log(res.data);
+
+    }
+    catch(err){
+      console.log(err);
+    }
+   }
+
+  const [count, setCount] = useState(0);
+  const [totalRounds, setTotalRounds] = useState(0);
+  
+  function increseCount(){
+    setCount(count+1);
+  }
+
+  function reset() {
+    setCount(0);
+    setTotalRounds(0);
+    localStorage.setItem('count', 0);
+    localStorage.setItem('totalRounds', 0);
+  }
+
+  useEffect(() => {
+    loadData();
+    const savedCount = localStorage.getItem('count');
+    const savedTotalRounds = localStorage.getItem('totalRounds');
+    if (savedCount) {
+      setCount(Number(savedCount));
+    }
+    if (savedTotalRounds) {
+      setTotalRounds(Number(savedTotalRounds));
+    }
+  }, []);
+
+
+  useEffect(()=>{
+    localStorage.setItem('count', count);
+    if(count===108){
+      setCount(0);
+      setTotalRounds(totalRounds+1);
+    }
+  }, [count])
+
+  
+  useEffect(()=>{
+    localStorage.setItem('totalRounds', totalRounds);
+  }, [totalRounds])
+
+
+  return (
+
+
+
+
+    <center className='container'>
+      <div className='mantra' >हरे कृष्ण हरे कृष्ण<br/>कृष्ण कृष्ण हरे हरे<br/>हरे राम हरे राम<br/>राम राम हरे हरे</div>
+      <center className='box'>
+         
+         <div className='count'>
+          <button disabled className='countButton'>-</button>
+          <div>{count}</div>
+          <button className='countButton' onClick={()=>increseCount()}>+</button>
+         </div>
+         <div>Total rounds: {totalRounds}</div>
+         <button className='resetButton' onClick={()=>reset()}>Reset</button>
+      </center>
+    </center>
+  )
+}
+
+export default App
